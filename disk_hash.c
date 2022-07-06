a26416ae3cc4e6007b831bdebfaba09e82625cbf
johnny <johnny_yang@openfind.com.tw>
2022-07-06 11:25:28 +0800

    Add open_table function, close_table function and head file
    第一次新增open_table, close_table function and head file.
    
    open_table裡做了目錄檢查，檔案(table, data)檢查,
    目錄不存在則新增目錄，存在則檢查是否為目錄,
    table不存在則新增初始化，存在則檢查是否正確,
    data不存在則新增.
    
    close_table裡把所有的檔案關閉且free array.
    
    head file存放一些頭部項目.

---
+++ disk_hash.c
@@ -0,0 +1,349 @@
+#include "disk_hash.h"
+#include "my_malloc.c"
+
+#ifdef _PRINTF_TIME
+   clock_t start, stop;
+#endif
+
+
+int main(){
+   #ifdef _PRINTF_TIME
+      start = clock();
+   #endif
+   #ifdef _PRINTF_TIME
+      stop = clock();
+      printf("first time : %f\n", ((double)(stop - start)/CLOCKS_PER_SEC));
+   #endif
+
+   _files fp;
+
+   #ifdef _PRINTF_TIME
+      start = clock();
+   #endif
+
+   open_table(&fp);
+
+   #ifdef _PRINTF_TIME
+      stop = clock();
+      printf("open_table time : %f\n", ((double)(stop - start)/CLOCKS_PER_SEC));
+   #endif
+
+   _DATA data;
+   data.key = my_malloc(10);
+   data.key = "asdqwe";
+   int res = 0;
+   find(data.key, &fp, &res);
+
+   close_table(&fp);
+
+
+   return 0;
+}
+
+int open_table(_files *fp){
+   struct stat info;
+
+   //dir exist?
+   if(stat("data", &info)){
+      if(errno == ENOENT){
+         umask(002);
+         if(mkdir("data", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)){
+            #ifdef _DEBUG
+            printf("%s\n", strerror(errno));
+            #endif
+
+            return errno;
+         }
+      }
+      else{
+         #ifdef _DEBUG
+         printf("%s\n", strerror(errno));
+         #endif
+
+         return errno;
+      }
+   }
+   //is dir?
+   else if(!S_ISDIR(info.st_mode)){
+      #ifdef _DEBUG
+      printf("Not dir!\n");
+      #endif
+
+      return ERR_DIR;
+   }
+
+   //file exist?
+   if(stat("data/table", &info)){
+      if(errno == ENOENT){
+         #ifdef _DEBUG
+         printf("Not table!\n");
+         #endif
+
+         fp->table = fopen("data/table", "w+");
+         if(!fp->table){
+            #ifdef _DEBUG
+            printf("ERROR open file!\n");
+            #endif
+
+            return ERR_OPEN_FILE;
+         }
+
+         fp->table_buf = my_malloc(TABLE_SIZE);
+         if(!fp->table_buf){
+             #ifdef _DEBUG
+               printf("ERROR my_malloc!\n");
+            #endif
+
+            close_table(fp);
+            return ERR_MY_MALLOC;
+         }
+
+         #ifdef _PRINTF_TIME
+            start = clock();
+         #endif
+
+         void *ptr = fp->table_buf;
+         for(; ptr <= fp->table_buf + TABLE_SIZE;){
+            //memset(ptr, '-', NEXT_SIZE);
+            *(int*)ptr = -1;
+            ptr += NEXT_SIZE;
+            *(char*)(ptr - 1) = '\n';
+         }
+
+         #ifdef _DEBUG
+            *(int*)fp->table_buf = 12345678;
+         #endif
+
+         #ifdef _PRINTF_TIME
+            stop = clock();
+            printf("for time : %f\n", ((double)(stop - start)/CLOCKS_PER_SEC));
+         #endif
+
+         #ifdef _PRINTF_TIME
+            start = clock();
+         #endif
+
+         if(TABLE_SIZE != fwrite(fp->table_buf, sizeof(char), TABLE_SIZE, fp->table)){
+            #ifdef _DEBUG
+            printf("ERROR WRITE!\n");
+            #endif
+
+            close_table(fp);
+            return ERR_WRITE;
+         }
+
+         #ifdef _PRINTF_TIME
+            stop = clock();
+            printf("fwrite time : %f\n", ((double)(stop - start)/CLOCKS_PER_SEC));
+         #endif
+
+         
+      }
+      else{
+         #ifdef _DEBUG
+         printf("%s\n", strerror(errno));
+         #endif
+
+         return errno;
+      }
+   }
+   else{
+      if(info.st_size != TABLE_SIZE){
+         #ifdef _DEBUG
+            printf("ERROR TABLE SIZE!\n");
+         #endif
+
+         return ERR_TABLE_SIZE;
+      }
+
+      fp->table = fopen("data/table", "r+");
+      if(!fp->table){
+         #ifdef _DEBUG
+         printf("ERROR open file!\n");
+         #endif
+
+         return ERR_OPEN_FILE;
+      }
+
+      fp->table_buf = my_malloc(TABLE_SIZE);
+      if(!fp->table_buf){
+         #ifdef _DEBUG
+            printf("ERROR my_malloc!\n");
+         #endif
+
+         close_table(fp);
+         return ERR_MY_MALLOC;
+      }
+
+      #ifdef _PRINTF_TIME
+         start = clock();
+      #endif
+
+      void *ptr = fp->table_buf;
+      if(TABLE_SIZE != fread(ptr, sizeof(char), TABLE_SIZE, fp->table)){
+         #ifdef _DEBUG
+            printf("ERROR fread!\n");
+         #endif
+
+         close_table(fp);
+         return ERR_READ_FILE;
+      }
+
+      #ifdef _PRINTF_TIME
+         stop = clock();
+         printf("fread time : %f\n", ((double)(stop - start)/CLOCKS_PER_SEC));
+      #endif
+
+      #ifdef _DEBUG
+         printf("------\n");
+         if(*(int*)fp->table_buf == -1){
+            printf("<->");
+         }
+         else{
+            printf("%d", *(int*)fp->table_buf);
+         }
+         printf("\n------\n");
+      #endif
+   }
+
+   if(stat("data/data", &info)){
+      if(errno == ENOENT){
+         fp->data = fopen("data/data", "w+");
+         if(!fp->data){
+            #ifdef _DEBUG
+            printf("ERROR open file!\n");
+            #endif
+
+            return ERR_OPEN_FILE;
+         }
+      }
+      else{
+         #ifdef _DEBUG
+            //printf("%s\n", strerror(errno));
+         #endif
+
+         return errno;
+      }
+   }
+   else{
+      fp->data = fopen("data/data", "r+");
+      if(!fp->data){
+         #ifdef _DEBUG
+            printf("ERROR open file!\n");
+         #endif
+
+         return ERR_OPEN_FILE;
+      }
+   }
+
+   
+   return SUCCESS;
+}
+
+int close_table(_files *fp){
+   fclose(fp->table);
+   fclose(fp->data);
+   my_free(fp->table_buf);
+
+
+   return SUCCESS;
+}
+
+/* int find(data.key char *key, _files *fp, int *result, _DATA *result_data){
+   int hash_num = hash_func(key);
+
+   void *table_ptr = (fp->table_buf + (hash_num * NEXT_SIZE));
+
+   if(*(int*)table_ptr == -1){
+      #ifdef _DEBUG
+         printf("NOT FOUND\n");
+         //*(int*)table_ptr = 654321;
+         //fseek(fp->table, 0, SEEK_SET);
+         //fp->table->_IO_write_ptr = fp->table->_IO_write_base;
+         //fwrite(fp->table_buf, sizeof(char), TABLE_SIZE, fp->table);
+      #endif
+      
+      *result = -1;
+      result_data = NULL;
+
+      return NOT_FOUND;
+   }
+   else{
+      #ifdef _DEBUG
+         printf("CAN FOUND\n");
+      #endif
+
+      void block_ptr = table_ptr;
+
+      void *data = my_malloc(BLOCK_SIZE);
+      if(!data){
+         #ifdef _DEBUG
+            printf("find my_malloc error!\n");
+         #endif
+
+         return ERR_MY_MALLOC;
+      }
+
+      size_t next = *(size_t*)(block_ptr + BLOCK_SIZE - NEXT_SIZE);
+      while(){
+         fseek(fp->data, *(int*)block_ptr, SEEK_SET);
+         if(BLOCK_SIZE != fread(data, sizeof(char), BLOCK_SIZE, fp->data)){
+            #ifdef _DEBUG
+               printf("find read error!\n");
+            #endif
+
+            return ERR_READ_FILE;
+         }
+
+         size_t block_total = *(size_t*)data;
+         void *data_ptr += data + sizeof(size_t);
+         for(; data_ptr <= data + block_total;){
+            void ptr = data_ptr;
+            _DATA data_temp;
+            data_temp.total_size = *(size_t*)ptr;
+            ptr += sizeof(size_t);
+            data.key = (char*)ptr;
+            ptr += strlen((char*)ptr) + 1;
+            data.val_size = *(size_t*)ptr;
+            ptr += sizeof(size_t);
+            data.val = (char*)ptr;
+            ptr += data.val_size;
+            data.type = *(char*)ptr;
+            ptr++;
+
+            
+
+            if(strcmp(key, data_temp.key)){
+               data_ptr = ptr;
+            }
+            else{
+               *result = *(int*)block_ptr;
+               result = data_ptr;
+               result_data = data_temp;
+
+               my_free(data);
+               return SUCCESS;
+            }
+
+         }   
+      }
+
+
+   }
+
+
+   return SUCCESS;
+
+} */
+
+int hash_func (const char* key){
+   char *str = (char*)key;
+   unsigned hash = offset_basis;
+   while(*str){
+      hash = hash * FNV_prime;
+      hash = hash ^ *str++;
+   }	
+
+
+   return (int)(hash % (TABLE_SIZE / NEXT_SIZE));
+}
---
+++ disk_hash.h
@@ -0,0 +1,62 @@
+#ifndef _FILE_H_
+#define _FILE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+
+#define BLOCK_SIZE         1 << 14
+#define BLOCK_TOTAL_SIZE   sizeof(size_t)
+#define NEXT_SIZE          sizeof(size_t)
+#define TABLE_SIZE         10000019 * NEXT_SIZE
+
+#define FNV_prime          16777619
+#define offset_basis       2166136261
+
+#define TYPE_INT           'I'
+#define TYPE_CHAR          'C'
+#define TYPE_BINARY        'B'
+
+
+#define SUCCESS            0
+#define ERR_BASE           0
+#define ERR_DIR            ERR_BASE - 1
+#define ERR_WRITE          ERR_BASE - 2
+#define ERR_OPEN_FILE      ERR_BASE - 3
+#define ERR_MY_MALLOC      ERR_BASE - 4
+#define ERR_TABLE_SIZE     ERR_BASE - 5
+#define ERR_READ_FILE      ERR_BASE - 6
+
+
+#define NOT_FOUND          ERR_BASE - 100
+
+
+typedef struct _DATA{
+   size_t   total_size;
+   char     *key;
+   size_t   val_size;
+   char     *val;
+   char     type;
+} _DATA;
+
+typedef struct _files{
+   FILE *table;
+   FILE *data;
+   void *table_buf;
+} _files;
+
+
+int open_table(_files *fp);
+int close_table(_files *fp);
+int add();
+int del();
+int find(const char *key, _files *fp, int *result);
+int reorganize();
+int hash_func();
+
+#endif
